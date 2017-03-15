eabc236a6139b7f7bf49522fcb913f87481bddaa
Kara Diaby <kdiaby.pro@gmail.com>
2017-03-15 17:18:49 +0100

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Text from "./text";
+
+export default class App extends Component {
+  render() {
+    return (
+
+    <div className="view-container">
+        <div className="view-inner">
+          <Text/>
+        </div>
+    </div>
+    );
+  }
+}
---
+++ src/components/text.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+class Text extends Component {
+  render() {
+    return(
+    <div>
+        <p>
+
+        Kara Diaby / Full Stack Developer | <a href="https://github.com/kara22" target="_blank">Github</a> | <a href="https://linkedin.com/in/karadiaby/" target="_blank">Linkedin</a> | <a href="mailto:kdiaby.pro@gmail.com">Mail</a>
+
+        </p>
+
+        <p className="text-center">Credits :
+      <ul className="list-inline">
+        <li><a href="https://youclone.herokuapp.com" target="_blank">Youclone - Personal project - React JS</a></li>
+        <li><a href="https://emcc-export.herokuapp.com" target="_blank">Vinci construction - B2B project - Ruby</a></li>
+      </ul>
+      </p>
+
+    </div>
+
+
+      );
+  }
+}
+
+export default Text;
