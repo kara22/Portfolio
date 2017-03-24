2725cab429f56c30bd8438e8bdb224ae56aa4067
Kara Diaby <kdiaby.pro@gmail.com>
2017-03-24 11:12:34 +0100

    some changes and heroku domain

--- src/components/text.js
+++ src/components/text.js
@@ -5,16 +5,31 @@ class Text extends Component {
     <div>
         <p>
 
-        Kara Diaby / Full Stack Developer | <a href="https://github.com/kara22" target="_blank">Github</a> | <a href="https://linkedin.com/in/karadiaby/" target="_blank">Linkedin</a> | <a href="mailto:kdiaby.pro@gmail.com">Mail</a>
+        Kara Diaby - Full Stack Developer | <a href="https://github.com/kara22" target="_blank">Github</a> | <a href="https://linkedin.com/in/karadiaby/" target="_blank">Linkedin</a> | <a href="mailto:kdiaby.pro@gmail.com">Mail</a>
 
         </p>
 
-        <p className="text-center">Credits :
-      <ul className="list-inline">
-        <li><a href="https://youclone.herokuapp.com" target="_blank">Youclone - Personal project - React JS</a></li>
-        <li><a href="https://emcc-export.herokuapp.com" target="_blank">Vinci construction - B2B project - Ruby</a></li>
+        <p className="text-center">Credits :</p>
+      <ul className="list-group">
+
+        <li className="text-center list-group-item">
+          <a href="https://markbin-app.herokuapp.com" target="_blank">Markbin - Personal project - Meteor / React JS</a>
+        </li>
+
+        <li className="text-center list-group-item">
+          <a href="https://employees-random.herokuapp.com" target="_blank">Employees - Personal project - Meteor / React JS</a>
+        </li>
+
+        <li className="text-center list-group-item">
+            <a href="https://youclone.herokuapp.com" target="_blank">Youclone - Personal project - React JS</a>
+        </li>
+
+        <li className="text-center list-group-item">
+          <a href="https://emcc-export.herokuapp.com" target="_blank">Vinci construction - B2B project - Ruby</a>
+        </li>
+
       </ul>
-      </p>
+
 
     </div>
 
